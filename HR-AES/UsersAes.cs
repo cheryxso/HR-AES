1465284498f42a6fad8b725d27b3cd49641b3a67
cheryxso <stor8ge18@gmail.com>
2024-11-27 00:40:34 +0200

    Create UsersAes.cs
    Create User model class with user types and basic account properties

---
+++ HR-AES/UsersAes.cs
@@ -0,0 +1,29 @@
+﻿namespace UserManagement.Models
+{
+    public enum UserType
+    {
+        HRManager,
+        HREmployee,
+        AESEmployee
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; } 
+        public UserType Type { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        public User(string username, string password, UserType type)
+        {
+            Username = username;
+            Password = password;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"Username: {Username}, Type: {Type}, Active: {IsActive}";
+        }
+    }
+}
